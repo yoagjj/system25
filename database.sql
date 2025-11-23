5997f625f4c8f11441d98fa1b1c5733e94047673
yoagjj <juliansyahyoga002@gmail.com>
2025-11-23 19:41:20 +0700

    Update database.sql

--- database.sql
+++ database.sql
@@ -27,119 +27,51 @@ SET time_zone = "+00:00";
 -- Table structure for table `barang`
 --
 
-CREATE TABLE `barang` (
-  `id` int NOT NULL,
-  `nama` varchar(50) NOT NULL,
-  `deskripsi` text NOT NULL,
-  `jumlah` int NOT NULL,
-  `jumlah_tersedia` int NOT NULL,
-  `lokasi` varchar(50) NOT NULL,
-  `kode` varchar(50) NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE DATABASE ukklv5;
+USE ukklv5;
 
---
--- Dumping data for table `barang`
---
+CREATE TABLE barang (
+  id int NOT NULL AUTO_INCREMENT,
+  nama varchar(50) NOT NULL,
+  deskripsi text NOT NULL,
+  jumlah int NOT NULL,
+  jumlah_tersedia int NOT NULL,
+  lokasi varchar(50) NOT NULL,
+  kode varchar(50) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-INSERT INTO `barang` (`id`, `nama`, `deskripsi`, `jumlah`, `jumlah_tersedia`, `lokasi`, `kode`, `created_at`) VALUES
+INSERT INTO barang 
+(id, nama, deskripsi, jumlah, jumlah_tersedia, lokasi, kode, created_at) VALUES
 (2, 'PC', 'dasd', 121, 121, '21', '21', '2025-11-21 14:09:45');
 
--- --------------------------------------------------------
+CREATE TABLE transaksi (
+  id int NOT NULL AUTO_INCREMENT,
+  id_barang int NOT NULL,
+  peminjam varchar(50) NOT NULL,
+  jumlah int NOT NULL,
+  kategori enum('pinjam','kembali') NOT NULL,
+  tanggal datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  catatan text NOT NULL,
+  PRIMARY KEY (id),
+  KEY id_barang (id_barang),
+  CONSTRAINT transaksi_ibfk_1 FOREIGN KEY (id_barang) REFERENCES barang (id) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Table structure for table `transaksi`
---
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT,
+  username varchar(50) NOT NULL,
+  fullname varchar(50) NOT NULL,
+  password varchar(255) NOT NULL,
+  created_at timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  role enum('admin','superadmin') DEFAULT 'admin',
+  PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
-CREATE TABLE `transaksi` (
-  `id` int NOT NULL,
-  `id_barang` int NOT NULL,
-  `peminjam` varchar(50) NOT NULL,
-  `jumlah` int NOT NULL,
-  `kategori` enum('pinjam','kembali') NOT NULL,
-  `tanggal` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `catatan` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `id` int NOT NULL,
-  `username` varchar(50) NOT NULL,
-  `fullname` varchar(50) NOT NULL,
-  `password` text NOT NULL,
-  `created_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `role` enum('admin','superadmin') CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci DEFAULT 'admin'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`id`, `username`, `fullname`, `password`, `created_at`, `role`) VALUES
+INSERT INTO users (id, username, fullname, password, created_at, role) VALUES
 (4, 'superadmin', 'Superadmin', '$2y$10$nKjRMtIAPgNSueu2wMv4NuMHylVJHaJxOzkHFE6PmhlsAC54d9OXC', '2025-11-21 09:15:34', 'superadmin'),
 (6, 'admin', 'Admin', '$2y$10$EUjYbXVXPf2BO9SHoJ/gke.ojEWc3jVR/Ep..EYBYnknnCEbKmVRa', '2025-11-21 14:07:30', 'admin');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `barang`
---
-ALTER TABLE `barang`
-  ADD PRIMARY KEY (`id`);
-
---
--- Indexes for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_barang` (`id_barang`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `barang`
---
-ALTER TABLE `barang`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `transaksi`
---
-ALTER TABLE `transaksi`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `transaksi`
---
-ALTER TABLE `transaksi`
-  ADD CONSTRAINT `transaksi_ibfk_1` FOREIGN KEY (`id_barang`) REFERENCES `barang` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
-COMMIT;
-
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
